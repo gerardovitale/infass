c9c58f70e3af1a4351284856d224ed2b4cccd3c0
gerardovitale <gerardo.vitale.errico@gmail.com>
2025-01-12 19:04:59 +0100

    add objectViewer role to bucket iam policy

--- tf-infra/bucket.tf
+++ tf-infra/bucket.tf
@@ -10,3 +10,13 @@ resource "google_storage_bucket" "infass_bucket" {
 
   labels = local.labels
 }
+
+resource "google_storage_bucket_iam_member" "bucket_permissions" {
+  for_each = toset([
+    "roles/storage.objectViewer",
+    "roles/storage.objectAdmin",
+  ])
+  bucket = google_storage_bucket.infass_bucket.name
+  member = "serviceAccount:${google_service_account.selenium_ingestor_sa.email}"
+  role   = each.value
+}
