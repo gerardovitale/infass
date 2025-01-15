7f9b0029b58111f8934fe29ee4a95ceae7e226c3
gerardovitale <gerardo.vitale.errico@gmail.com>
2025-01-15 14:34:53 +0100

    revert custom role creation

--- tf-infra/ingestor.tf
+++ tf-infra/ingestor.tf
@@ -1,29 +1,19 @@
 # Service Account for Cloud Run job
 resource "google_service_account" "selenium_ingestor_sa" {
   account_id   = "${var.APP_NAME}-selenium-ingestor"
-  description  = "Selenium Ingestor Service Account created by terraform"
+  description = "Selenium Ingestor Service Account created by terraform"
   display_name = "Cloud Run Job Service Account for Selenium Ingestor"
 }
 
-# Custom role
-resource "google_project_iam_custom_role" "custom_storage_role" {
-  role_id     = "${var.APP_NAME}_custom_storage_role"
-  title       = "Custom Storage Role for ${var.APP_NAME}"
-  description = "Custom role to grant specific storage permissions"
-  project     = var.PROJECT
-  permissions = [
-    "storage.buckets.get",
-    "storage.objects.create",
-    "storage.objects.get",
-    "storage.objects.list",
-  ]
-}
-
-# SA and Custom role binding
+# Grant necessary permissions
 resource "google_project_iam_member" "cloud_run_job_storage_permissions" {
+  for_each = toset([
+    "roles/storage.objectCreator",
+    "roles/storage.objectViewer",
+  ])
   project = var.PROJECT
   member  = "serviceAccount:${google_service_account.selenium_ingestor_sa.email}"
-  role    = google_project_iam_custom_role.custom_storage_role.name
+  role    = each.value
 }
 
 # Job Definition
