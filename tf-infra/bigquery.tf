613437446769a2687ace9e14c0cb1ab441e89a5f
gerardovitale <gerardo.vitale.errico@gmail.com>
2025-01-26 17:30:15 +0100

    add access role to bigquery table

--- tf-infra/bigquery.tf
+++ tf-infra/bigquery.tf
@@ -5,4 +5,9 @@ resource "google_bigquery_dataset" "infass_dataset" {
   friendly_name = "Infass Dataset"
   description   = "Dataset for storing inflation assistant data"
   labels        = local.labels
+
+  access {
+    role          = "OWNER"
+    user_by_email = google_service_account.pandas_transformer_sa.email
+  }
 }
