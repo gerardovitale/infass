51084009c4d76efaedd68fefac7dcdee58c94dad
gerardovitale <gerardo.vitale.errico@gmail.com>
2025-01-26 19:18:45 +0100

    remove unneeded TRANSFORMER_LIMIT

--- tf-infra/transformer.tf
+++ tf-infra/transformer.tf
@@ -37,10 +37,6 @@ resource "google_cloud_run_v2_job" "pandas_transformer_job" {
             memory = "1Gi"
           }
         }
-        env {
-          name  = "TRANSFORMER_LIMIT"
-          value = 2
-        }
       }
     }
   }
