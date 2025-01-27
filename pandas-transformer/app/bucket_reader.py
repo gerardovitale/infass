ce79caa750565857a6fdf7ccc3ef7647725d1329
gerardovitale <gerardo.vitale.errico@gmail.com>
2025-01-27 16:46:15 +0100

    improve bucket reader

--- pandas-transformer/app/bucket_reader.py
+++ pandas-transformer/app/bucket_reader.py
@@ -9,13 +9,11 @@ from google.cloud import storage
 logger = logging.getLogger(__name__)
 
 
-def _list_blobs(bucket_name):
-    storage_client = storage.Client()
-    return storage_client.list_blobs(bucket_name)
+def _list_blobs(storage_client: storage.Client, bucket_name: str):
+    return storage_client.list_blobs(bucket_name, prefix="merc/")
 
 
-def _read_blobs(bucket_name, blob_name):
-    storage_client = storage.Client()
+def _read_blobs(storage_client: storage.Client, bucket_name: str, blob_name: str):
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(blob_name)
     with blob.open("r") as f:
@@ -25,9 +23,10 @@ def _read_blobs(bucket_name, blob_name):
 def read_csv_as_pd_df(bucket_name: str, limit: int | None) -> pd.DataFrame:
     logger.info(f"Getting objects from {bucket_name} with limit: {limit}")
     df_list = []
-    blobs = _list_blobs(bucket_name)
+    storage_client = storage.Client()
+    blobs = _list_blobs(storage_client, bucket_name)
 
-    blobs = sorted([b for b in blobs if b.name.endswith(".csv")], key=lambda b: b.name, reverse=True)
+    blobs = sorted(blobs, key=lambda b: b.name, reverse=True)
     if limit:
         if limit == -1:
             logger.info("Reading only the most recent object")
@@ -38,8 +37,9 @@ def read_csv_as_pd_df(bucket_name: str, limit: int | None) -> pd.DataFrame:
 
     for b in blobs:
         logger.info(f"Reading {b.name}")
-        string_csv_data = _read_blobs(bucket_name, b.name)
+        string_csv_data = _read_blobs(storage_client, bucket_name, b.name)
         csv_data = StringIO(string_csv_data)
         df_list.append(pd.read_csv(csv_data))
 
+    logger.info(f"Read {len(df_list)} objects, with max and min: {max(df_list)} and {min(df_list)}")
     return pd.concat(df_list).reset_index().drop(columns=["index"])
--- pandas-transformer/tests/test_bucket_reader.py
+++ pandas-transformer/tests/test_bucket_reader.py
@@ -1,9 +1,6 @@
-from unittest.mock import (
-    patch,
-)
+from unittest.mock import patch
 
 import pandas as pd
-
 from bucket_reader import read_csv_as_pd_df
 from tests.conf_test import BasicTestCase
 
@@ -23,6 +20,10 @@ class TestBucketReader(BasicTestCase):
         self.addCleanup(logger_patch.stop)
         self.mock_logger = logger_patch.start()
 
+        storage_client_patch = patch(f"{TEST_MODULE}.storage.Client")
+        self.addCleanup(storage_client_patch.stop)
+        self.mock_storage_client = storage_client_patch.start()
+
         list_blobs_patch = patch(f"{TEST_MODULE}._list_blobs")
         self.addCleanup(list_blobs_patch.stop)
         self.mock_list_blobs = list_blobs_patch.start()
@@ -32,56 +33,53 @@ class TestBucketReader(BasicTestCase):
         self.mock_read_blobs = read_blobs_patch.start()
 
     def test_read_csv_as_pd_df_should_return_last_object_when_limit_is_minus_1(self):
-        test_bucket_name = 'test_bucket_name'
+        test_bucket_name = "test_bucket_name"
         limit = -1
-        self.mock_list_blobs.return_value = (
-            MockBlob(f"2025-01-0{day}.csv", "mock_content")
-            for day in range(1, 5)
-        )
+        self.mock_list_blobs.return_value = (MockBlob(f"2025-01-0{day}.csv", "mock_content") for day in range(1, 5))
         self.mock_read_blobs.return_value = "col1,col2,col3\n1,2,3\n1,2,3\n1,2,3"
 
-        expected = pd.DataFrame({
-            "col1": [1, 1, 1],
-            "col2": [2, 2, 2],
-            "col3": [3, 3, 3],
-        })
+        expected = pd.DataFrame(
+            {
+                "col1": [1, 1, 1],
+                "col2": [2, 2, 2],
+                "col3": [3, 3, 3],
+            }
+        )
         actual = read_csv_as_pd_df(test_bucket_name, limit)
 
         self.assertIsInstance(actual, pd.DataFrame)
         self.assert_pandas_dataframes_equal(expected, actual)
 
     def test_read_csv_as_pd_df_should_return_all_objects_when_limit_is_none(self):
-        test_bucket_name = 'test_bucket_name'
-        self.mock_list_blobs.return_value = (
-            MockBlob(f"2025-01-0{day}.csv", "mock_content")
-            for day in range(1, 4)
-        )
+        test_bucket_name = "test_bucket_name"
+        self.mock_list_blobs.return_value = (MockBlob(f"2025-01-0{day}.csv", "mock_content") for day in range(1, 4))
         self.mock_read_blobs.return_value = "col1,col2,col3\n1,2,3\n1,2,3\n1,2,3"
 
-        expected = pd.DataFrame({
-            "col1": [1, 1, 1, 1, 1, 1, 1, 1, 1],
-            "col2": [2, 2, 2, 2, 2, 2, 2, 2, 2],
-            "col3": [3, 3, 3, 3, 3, 3, 3, 3, 3],
-        })
+        expected = pd.DataFrame(
+            {
+                "col1": [1, 1, 1, 1, 1, 1, 1, 1, 1],
+                "col2": [2, 2, 2, 2, 2, 2, 2, 2, 2],
+                "col3": [3, 3, 3, 3, 3, 3, 3, 3, 3],
+            }
+        )
         actual = read_csv_as_pd_df(test_bucket_name, None)
 
         self.assertIsInstance(actual, pd.DataFrame)
         self.assert_pandas_dataframes_equal(expected, actual)
 
     def test_read_csv_as_pd_df_should_return_n_objects_when_limit_is_n(self):
-        test_bucket_name = 'test_bucket_name'
+        test_bucket_name = "test_bucket_name"
         limit = 2
-        self.mock_list_blobs.return_value = (
-            MockBlob(f"2025-01-0{day}.csv", "mock_content")
-            for day in range(1, 8)
-        )
+        self.mock_list_blobs.return_value = (MockBlob(f"2025-01-0{day}.csv", "mock_content") for day in range(1, 8))
         self.mock_read_blobs.return_value = "col1,col2,col3\n1,2,3\n1,2,3\n1,2,3"
 
-        expected = pd.DataFrame({
-            "col1": [1, 1, 1, 1, 1, 1],
-            "col2": [2, 2, 2, 2, 2, 2],
-            "col3": [3, 3, 3, 3, 3, 3],
-        })
+        expected = pd.DataFrame(
+            {
+                "col1": [1, 1, 1, 1, 1, 1],
+                "col2": [2, 2, 2, 2, 2, 2],
+                "col3": [3, 3, 3, 3, 3, 3],
+            }
+        )
         actual = read_csv_as_pd_df(test_bucket_name, limit)
 
         self.assertIsInstance(actual, pd.DataFrame)
