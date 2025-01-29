555a53d5f914bc2f3763169626d8b0e9ccf964e7
gerardovitale <gerardo.vitale.errico@gmail.com>
2025-01-29 19:07:01 +0100

    imporve alert

--- tf-infra/alerts.tf
+++ tf-infra/alerts.tf
@@ -9,19 +9,22 @@ resource "google_monitoring_notification_channel" "email" {
 resource "google_monitoring_alert_policy" "job_failure_alert" {
   display_name = "Cloud Run Job Failure Alert"
   combiner     = "OR"
+  severity     = "ERROR"
+
   conditions {
     display_name = "Job Failed Condition"
     condition_threshold {
-      filter          = "metric.type=\"run.googleapis.com/job/completed_execution_count\" AND resource.type=\"cloud_run_job\" AND metric.labels.result=\"failed\""
+      filter          = "resource.type = \"cloud_run_job\" AND metric.type = \"run.googleapis.com/job/completed_execution_count\" AND metric.labels.result = \"failed\""
       comparison      = "COMPARISON_GT"
       threshold_value = 0
-      duration        = "60s"
+      duration        = "10s"
       aggregations {
         alignment_period   = "60s"
         per_series_aligner = "ALIGN_RATE"
       }
     }
   }
+
   notification_channels = [google_monitoring_notification_channel.email.name]
   user_labels           = local.labels
 }
