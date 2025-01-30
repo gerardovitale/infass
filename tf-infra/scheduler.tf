82c877dae8da93d188a25591304a96bb11edce94
gerardovitale <gerardo.vitale.errico@gmail.com>
2025-01-30 16:20:48 +0100

    add a second scheduler for the transformer

--- tf-infra/scheduler.tf
+++ tf-infra/scheduler.tf
@@ -9,9 +9,10 @@ resource "google_project_iam_member" "cloud_scheduler_run_invoker" {
   member  = "serviceAccount:${google_service_account.cloud_scheduler_sa.email}"
 }
 
-resource "google_cloud_scheduler_job" "trigger_cloud_run_job" {
-  name        = "${var.APP_NAME}-trigger-cloud-run-job"
-  description = "Trigger Cloud Run Job at 5 AM daily"
+
+resource "google_cloud_scheduler_job" "trigger_ingestion_job_daily" {
+  name        = "${var.APP_NAME}-trigger-ingestion-job-daily"
+  description = "Trigger Ingestion Cloud Run Job at 5 AM daily"
   region      = "europe-west1"
   schedule    = "0 8 * * *"
   time_zone   = "Europe/Madrid" #"Etc/UTC"
@@ -19,6 +20,23 @@ resource "google_cloud_scheduler_job" "trigger_cloud_run_job" {
   http_target {
     http_method = "POST"
     uri         = "https://${var.REGION}-run.googleapis.com/apis/run.googleapis.com/v1/namespaces/${var.PROJECT}/jobs/${google_cloud_run_v2_job.selenium_ingestor_job.name}:run"
+    oauth_token {
+      service_account_email = google_service_account.cloud_scheduler_sa.email
+    }
+  }
+}
+
+
+resource "google_cloud_scheduler_job" "trigger_transformer_weekly" {
+  name        = "${var.APP_NAME}-trigger-transformer-job-weekly"
+  description = "Trigger Transformer Cloud Run Job every Saturday at 8 AM UTC"
+  region      = "europe-west1"
+  schedule    = "0 8 * * 6"     # ⏰ Runs every Saturday at 8:00 AM UTC
+  time_zone   = "Europe/Madrid" #"Etc/UTC"
+
+  http_target {
+    http_method = "POST"
+    uri         = "https://${var.REGION}-run.googleapis.com/apis/run.googleapis.com/v1/namespaces/${var.PROJECT}/jobs/${google_cloud_run_v2_job.pandas_transformer_job.name}:run"
     oauth_token {
       service_account_email = google_service_account.cloud_scheduler_sa.email
     }
