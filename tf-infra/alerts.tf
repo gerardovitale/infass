6b56f683c92a563982622c4550feef5a99140f4c
gerardovitale <gerardo.vitale.errico@gmail.com>
2025-02-11 18:28:11 +0100

    add alert for the bigquery schedule job

--- tf-infra/alerts.tf
+++ tf-infra/alerts.tf
@@ -6,7 +6,7 @@ resource "google_monitoring_notification_channel" "email" {
   }
 }
 
-resource "google_monitoring_alert_policy" "job_failure_alert" {
+resource "google_monitoring_alert_policy" "clour_run_job_failure_alert" {
   display_name = "Cloud Run Job Failure Alert"
   combiner     = "OR"
   severity     = "ERROR"
@@ -15,6 +15,29 @@ resource "google_monitoring_alert_policy" "job_failure_alert" {
     display_name = "Job Failed Condition"
     condition_threshold {
       filter          = "resource.type = \"cloud_run_job\" AND metric.type = \"run.googleapis.com/job/completed_execution_count\" AND metric.labels.result = \"failed\""
+      comparison      = "COMPARISON_GT"
+      threshold_value = 0
+      duration        = "0s"
+      aggregations {
+        alignment_period   = "60s"
+        per_series_aligner = "ALIGN_RATE"
+      }
+    }
+  }
+
+  notification_channels = [google_monitoring_notification_channel.email.name]
+  user_labels           = local.labels
+}
+
+resource "google_monitoring_alert_policy" "bq_job_failure_alert" {
+  display_name = "BigQuery Scheduled Job Failure Alert"
+  combiner     = "OR"
+  severity     = "ERROR"
+
+  conditions {
+    display_name = "BigQuery Scheduled Job Failed"
+    condition_threshold {
+      filter          = "resource.type = \"bigquery_dts_config\" AND metric.type = \"bigquerydatatransfer.googleapis.com/transfer_count\" AND metric.labels.state = \"FAILED\""
       comparison      = "COMPARISON_GT"
       threshold_value = 0
       duration        = "0s"
