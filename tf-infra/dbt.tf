0b175be05b0032f482aac5cbb27458c9394f9d57
gerardovitale <gerardo.vitale.errico@gmail.com>
2025-02-12 18:22:13 +0100

    add dbt sa in tf

---
+++ tf-infra/dbt.tf
@@ -0,0 +1,17 @@
+# Service Account for DBT
+resource "google_service_account" "bigquery_schedule_job_sa" {
+  account_id   = "${var.APP_NAME}-dbt"
+  description  = "DBT Service Account created by terraform"
+  display_name = "DBT Service Account"
+}
+
+# Grant necessary permissions to the DBT service account
+resource "google_project_iam_member" "dbt_permissions" {
+  for_each = toset([
+    "roles/bigquery.jobUser",
+    "roles/bigquery.dataEditor",
+  ])
+  project = var.PROJECT
+  member  = "serviceAccount:${google_service_account.bigquery_schedule_job_sa.email}"
+  role    = each.value
+}
