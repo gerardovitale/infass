c77d6537f3d6879f599cc6685b4ac9cda67ff9ac
gerardovitale <gerardo.vitale.errico@gmail.com>
2025-05-15 12:46:31 +0200

    move bigquery schedule queries to dbt run on GitHub Actions

--- infra/alerts.tf
+++ infra/alerts.tf
@@ -28,26 +28,3 @@ resource "google_monitoring_alert_policy" "clour_run_job_failure_alert" {
   notification_channels = [google_monitoring_notification_channel.email.name]
   user_labels           = local.labels
 }
-
-# resource "google_monitoring_alert_policy" "bq_job_failure_alert" {
-#   display_name = "BigQuery Scheduled Query Failure Alert"
-#   combiner     = "OR"
-#   severity     = "ERROR"
-
-#   conditions {
-#     display_name = "BigQuery Scheduled Job Failed"
-#     condition_threshold {
-#       filter          = "resource.type = \"bigquery_dts_config\" AND metric.type = \"bigquerydatatransfer.googleapis.com/transfer_run_execution_count\" AND metric.labels.status = \"FAILED\""
-#       comparison      = "COMPARISON_GT"
-#       threshold_value = 0
-#       duration        = "0s"
-#       aggregations {
-#         alignment_period   = "60s"
-#         per_series_aligner = "ALIGN_RATE"
-#       }
-#     }
-#   }
-
-#   notification_channels = [google_monitoring_notification_channel.email.name]
-#   user_labels           = local.labels
-# }
