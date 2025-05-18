5097d665e45f9f7f1e7103f77a6eec80e601d43f
gerardovitale <gerardo.vitale.errico@gmail.com>
2025-05-18 16:15:04 +0200

    infra: add new test dataset for dbt testing

--- infra/bigquery.tf
+++ infra/bigquery.tf
@@ -13,6 +13,25 @@ resource "google_bigquery_dataset" "infass_dataset" {
   }
 
   access {
+    role          = "READER"
+    user_by_email = var.GCP_USER
+  }
+}
+
+resource "google_bigquery_dataset" "infass_test_dataset" {
+  dataset_id    = "test_infass"
+  project       = var.PROJECT
+  location      = "EU"
+  friendly_name = "Infass Test Dataset"
+  description   = "Dataset for testing dbt transformations"
+  labels        = local.labels
+
+  access {
+    role          = "OWNER"
+    user_by_email = google_service_account.pandas_transformer_sa.email
+  }
+
+  access {
     role          = "OWNER"
     user_by_email = var.GCP_USER
   }
