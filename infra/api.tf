3264ced7b2545ac43bf69b63789c738a1edb672a
gerardovitale <gerardo.vitale.errico@gmail.com>
2025-06-06 17:46:43 +0200

    infra: add api resources

---
+++ infra/api.tf
@@ -0,0 +1,95 @@
+# ------------------------------
+# Cloud Storage Bucket
+# ------------------------------
+resource "google_storage_bucket" "sqlite_bucket" {
+  name          = "${var.APP_NAME}-sqlite-bucket"
+  location      = var.REGION
+  storage_class = "STANDARD"
+
+  versioning {
+    enabled = true
+  }
+
+  labels = local.labels
+}
+
+# ------------------------------
+# Service Account for Cloud Run
+# ------------------------------
+resource "google_service_account" "cloud_run_sa" {
+  account_id   = "${var.APP_NAME}-cloud-run-sa"
+  display_name = "Cloud Run Service Account for ${var.APP_NAME}"
+}
+
+resource "google_storage_bucket_iam_member" "run_bucket_access" {
+  bucket = google_storage_bucket.sqlite_bucket.name
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:${google_service_account.cloud_run_sa.email}"
+}
+
+# ------------------------------
+# Cloud Run API Service
+# ------------------------------
+resource "google_cloud_run_v2_service" "api_service" {
+  name     = "${var.APP_NAME}-api-service"
+  location = var.REGION
+  ingress  = "INGRESS_TRAFFIC_INTERNAL_ONLY"
+
+  template {
+    timeout         = "5s"
+    service_account = google_service_account.cloud_run_sa.email
+
+    containers {
+      image = "docker.io/${var.DOCKER_HUB_USERNAME}/infass-api:${var.DOCKER_IMAGE_TAG}"
+
+      volume_mounts {
+        name       = "sqlite-vol"
+        mount_path = "/mnt/sqlite"
+      }
+    }
+
+    volumes {
+      name = "sqlite-vol"
+      gcs {
+        bucket    = google_storage_bucket.sqlite_bucket.name
+        read_only = true
+      }
+    }
+
+  }
+
+  labels = local.labels
+}
+
+# ------------------------------
+# Cloud Run Job (ETL)
+# ------------------------------
+resource "google_cloud_run_v2_job" "reversed_etl_job" {
+  name     = "${var.APP_NAME}-reversed-etl-job"
+  location = var.REGION
+
+  template {
+    template {
+      timeout         = "300s"
+      max_retries     = 0
+      service_account = google_service_account.cloud_run_sa.email
+
+      containers {
+        image = "docker.io/${var.DOCKER_HUB_USERNAME}/infass-reversed_etl:${var.DOCKER_IMAGE_TAG}"
+        volume_mounts {
+          name       = "sqlite-vol"
+          mount_path = "/mnt/sqlite"
+        }
+      }
+
+      volumes {
+        name = "sqlite-vol"
+        gcs {
+          bucket = google_storage_bucket.sqlite_bucket.name
+        }
+      }
+    }
+  }
+
+  labels = local.labels
+}
