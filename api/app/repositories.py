89decd24501df4767ad3f3e4fec4b5912f78fff1
gerardovitale <gerardo.vitale.errico@gmail.com>
2025-06-10 16:30:52 +0200

    api: add get_connection and check_db_path_exist methods

--- api/app/repositories.py
+++ api/app/repositories.py
@@ -2,6 +2,8 @@ import logging
 import sqlite3
 from abc import ABC
 from abc import abstractmethod
+from pathlib import Path
+from sqlite3 import Connection
 from sqlite3 import Cursor
 
 from google.cloud.bigquery import Client
@@ -19,15 +21,12 @@ class ProductRepository(ABC):
 class SQLiteProductRepository(ProductRepository):
 
     def __init__(self, db_path: str):
+        self.check_db_path_exist(db_path)
         self.db_path = db_path
-
-    @staticmethod
-    def _get_column_names(cursor: Cursor) -> list[str]:
-        return [column[0] for column in cursor.description]
 
     def search_products(self, search_term: str) -> list[dict]:
         logger.info(f"SQLite: Searching products with term '{search_term}'")
-        conn = sqlite3.connect(self.db_path)
+        conn = self.get_connection()
         cursor = conn.cursor()
         query = """
                 SELECT
@@ -49,6 +48,24 @@ class SQLiteProductRepository(ProductRepository):
         logger.info(f"SQLite: Found {len(rows)} products for term '{search_term}'")
         conn.close()
         return [dict(zip(self._get_column_names(cursor), row)) for row in rows]
+
+    def get_connection(self) -> Connection:
+        logger.info(f"SQLite: Connecting to database at {self.db_path}")
+        try:
+            return sqlite3.connect(self.db_path)
+        except sqlite3.OperationalError:
+            logger.error(f"SQLite: Failed to connect to database at {self.db_path}")
+            raise sqlite3.OperationalError("Could not connect to the SQLite database.")
+
+    @staticmethod
+    def check_db_path_exist(db_path: str) -> None:
+        if not Path(db_path).exists():
+            logger.error(f"SQLite: Database file not found at {db_path}")
+            raise FileNotFoundError(f"Database file not found at {db_path}")
+
+    @staticmethod
+    def _get_column_names(cursor: Cursor) -> list[str]:
+        return [column[0] for column in cursor.description]
 
 
 class BigQueryProductRepository(ProductRepository):
