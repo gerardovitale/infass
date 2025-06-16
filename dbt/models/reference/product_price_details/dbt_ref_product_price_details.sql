f1d901d853fe3ed21b804adb799b7b76b37a850b
gerardovitale <gerardo.vitale.errico@gmail.com>
2025-06-17 00:10:02 +0200

    dbt: make ref products price detials incremental

--- dbt/models/reference/product_price_details/dbt_ref_product_price_details.sql
+++ dbt/models/reference/product_price_details/dbt_ref_product_price_details.sql
@@ -1,6 +1,13 @@
 {{
   config(
-    materialized = "table",
+    materialized = 'incremental',
+    unique_key = ['id', 'date'],
+    merge_update_columns = [
+      'price',
+      'sma7',
+      'sma15',
+      'sma30',
+    ],
     partition_by = {
       "field": "date",
       "data_type": "date",
@@ -15,9 +22,13 @@ SELECT
     merc.price,
     merc.price_ma_7 AS sma7,
     merc.price_ma_15 AS sma15,
-    merc.price_ma_30 AS sma30
+    merc.price_ma_30 AS sma30,
+    CAST({{ current_timestamp() }} AS DATE) AS created_at,
+    CAST({{ current_timestamp() }} AS DATE) AS updated_at
 FROM {{ source('infass', 'merc') }} AS merc
 LEFT JOIN {{ ref('dbt_ref_products') }} AS products
-    ON products.name = merc.name AND products.size = merc.size
-WHERE
-    date BETWEEN DATE_SUB({{ get_last_saturday_date() }}, INTERVAL '6' MONTH) AND {{ get_last_saturday_date() }}
+  ON products.name = merc.name
+  AND products.size = merc.size
+{% if is_incremental() %}
+WHERE date >= (SELECT MAX(date) FROM {{ this }})
+{% endif %}
