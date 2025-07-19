c038d1f03772ef43bcacdf009f8756671887da65
Gerardo Vitale <gerardo.vitale.errico@gmail.com>
2025-07-19 13:16:06 +0200

    dbt: improve dbt_ref_products.sql model

--- dbt/models/reference/products/dbt_ref_products.sql
+++ dbt/models/reference/products/dbt_ref_products.sql
@@ -3,20 +3,17 @@ SELECT
     TO_HEX(MD5(CONCAT(name, size))) AS id,
     name,
     size,
-    STRING_AGG(category, ' | ') AS categories,
-    STRING_AGG(subcategory, ' | ') AS subcategories,
+    STRING_AGG(DISTINCT category, ' | ') AS categories,
+    STRING_AGG(DISTINCT subcategory, ' | ') AS subcategories,
     MAX_BY(price, date) AS price,
     MAX_BY(image_url, date) AS image_url
 FROM {{ source('infass', 'merc') }}
 WHERE
-    date = {{ get_last_saturday_date() }}
-    AND category IS NOT NULL
+    category IS NOT NULL
     AND subcategory IS NOT NULL
-
     {% if is_incremental() %}
     AND TO_HEX(MD5(CONCAT(name, size))) NOT IN (SELECT id FROM {{ this }})
     {% endif %}
-
 GROUP BY
     name,
     size
