44a907c23fcc766f3a37b2d0c96feea6990b5faa
gerardovitale <gerardo.vitale.errico@gmail.com>
2025-08-25 22:14:21 +0200

    transformerV2: fix new merc schema

--- transformer-v2/app/schemas.py
+++ transformer-v2/app/schemas.py
@@ -9,9 +9,6 @@ MERC_SCHEMA = [
     SchemaField("subcategory", "STRING"),
     SchemaField("image_url", "STRING"),
     SchemaField("price", "FLOAT"),
-    SchemaField("price_ma_7", "FLOAT"),
-    SchemaField("price_ma_15", "FLOAT"),
-    SchemaField("price_ma_30", "FLOAT"),
     SchemaField("original_price", "FLOAT"),
     SchemaField("discount_price", "FLOAT"),
 ]
