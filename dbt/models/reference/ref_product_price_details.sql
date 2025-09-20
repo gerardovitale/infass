04723ac372ecf8930e159cae5335952a676ec586
gerardovitale <gerardo.vitale.errico@gmail.com>
2025-09-20 12:36:36 +0200

    dbt: create new reference tables based on new star models with same schemas

---
+++ dbt/models/reference/ref_product_price_details.sql
@@ -0,0 +1,65 @@
+{{
+  config(
+    materialized = 'incremental',
+    incremental_strategy = 'insert_overwrite',
+    unique_key = ['id', 'date'],
+    partition_by = {
+      "field": "date",
+      "data_type": "date",
+      "granularity": "day"
+    },
+    cluster_by = ["id"]
+  )
+}}
+
+{% set max_window = 30 %}
+{% set backfill_days = var('sma_backfill_days', max_window) %}
+
+WITH base AS (
+    SELECT
+        d.date,
+        fact.product_key AS id,
+        fact.price,
+    FROM {{ ref('fact_product_price_daily') }} AS fact
+    LEFT JOIN {{ ref('dim_date') }} AS d
+        ON d.date_key = fact.date_key
+    {% if is_incremental() %}
+    WHERE d.date >= DATE_SUB(CURRENT_DATE(), INTERVAL {{ backfill_days }} DAY)
+    {% endif %}
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY d.date, fact.product_key ORDER BY d.date DESC) = 1
+),
+
+sma AS (
+    SELECT
+        date,
+        id,
+        price,
+
+        AVG(price) OVER (
+            PARTITION BY id
+            ORDER BY UNIX_DATE(date)
+            RANGE BETWEEN 6 PRECEDING AND CURRENT ROW
+        ) AS sma7,
+
+        AVG(price) OVER (
+            PARTITION BY id
+            ORDER BY UNIX_DATE(date)
+            RANGE BETWEEN 14 PRECEDING AND CURRENT ROW
+        ) AS sma15,
+
+        AVG(price) OVER (
+            PARTITION BY id
+            ORDER BY UNIX_DATE(date)
+            RANGE BETWEEN 29 PRECEDING AND CURRENT ROW
+        ) AS sma30
+    FROM base
+)
+
+SELECT
+    date,
+    id,
+    price,
+    sma7,
+    sma15,
+    sma30
+FROM sma
---
+++ dbt/models/reference/ref_products.sql
@@ -0,0 +1,39 @@
+{{ config(
+    materialized = 'incremental',
+    incremental_strategy = 'merge',
+    unique_key = 'id',
+    on_schema_change = 'sync',
+    cluster_by = ['id']
+) }}
+
+{% set recent_days = var('ref_latest_price_days', 120) %}
+
+WITH src AS (
+    SELECT
+        product_key,
+        STRING_AGG(DISTINCT category, ' | ') AS categories,
+        STRING_AGG(DISTINCT subcategory, ' | ') AS subcategories,
+        MAX_BY(price, date) AS price,
+    FROM {{ ref('fact_product_price_daily') }}
+    JOIN {{ ref('dim_date') }} USING (date_key)
+    LEFT JOIN {{ ref ('dim_category') }} USING (category_key)
+    WHERE
+        category IS NOT NULL
+        AND subcategory IS NOT NULL
+        {% if is_incremental() %}
+        AND date >= DATE_SUB(CURRENT_DATE(), INTERVAL {{ recent_days }} DAY)
+        {% endif %}
+    GROUP BY product_key
+)
+
+SELECT
+    src.product_key AS id,
+    p.name,
+    p.size,
+    src.categories,
+    src.subcategories,
+    src.price,
+    p.image_url
+FROM src
+LEFT JOIN {{ ref('dim_product') }} AS p
+    ON src.product_key = p.product_key
